3993baf8f1276c4a9c27e58ba41aadbe0378e82b
Matthew Powelson <matthew.powelson@swri.org>
2019-05-15 11:38:44 -0500

    Add Simple IPOPT Example
    This solves the same problem as pagmo_example, Minimize( x^2 + 5 )

---
+++ trajopt_examples/src/pagmo_ipopt_example.cpp
@@ -0,0 +1,87 @@
+// Pagmo
+#include <pagmo/algorithm.hpp>
+#include <pagmo/algorithms/sade.hpp>
+#include <pagmo/algorithms/pso.hpp>
+#include <pagmo/archipelago.hpp>
+#include <pagmo/problem.hpp>
+#include <pagmo/utils/gradients_and_hessians.hpp>
+
+#define HAVE_CSTDDEF
+#include <IpTNLP.hpp>
+#undef HAVE_CSTDDEF
+
+#include <pagmo/algorithms/ipopt.hpp>
+
+// From UDP
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <pagmo/exceptions.hpp>
+#include <pagmo/problem.hpp>  // needed for cereal registration macro
+#include <pagmo/types.hpp>
+
+namespace pagmo
+{
+struct squared
+{
+  squared(){};
+
+  vector_double fitness(const vector_double& x) const
+  {
+    vector_double f(1, 0.);
+
+    f[0] = x[0] * x[0] + 5;
+    return f;
+  }
+  std::pair<vector_double, vector_double> get_bounds() const
+  {
+    vector_double lb(1, -500);
+    vector_double ub(1, 500);
+    return { lb, ub };
+  }
+
+  // Note that IPOPT requires gradient information. Because of this, we will approximate it numerically using PAGMO's
+  // utilities
+  bool has_gradient() const { return true; }
+  vector_double gradient(const vector_double& x) const
+  {
+    using FTYPE = std::function<vector_double(const vector_double&)>;
+    return pagmo::estimate_gradient<FTYPE>(std::bind(&squared::fitness, this, std::placeholders::_1), x);
+  }
+};
+}  // namespace pagmo
+
+int main(int argc, char** argv)
+{
+  std::cout << "Beginning Optimization \n";
+
+  // 1 - Instantiate a pagmo problem constructing it from a UDP
+  pagmo::squared tmp;
+  pagmo::problem pagmo_prob(tmp);
+
+  // 2 - Create a User Defined Algorithm (UDA)
+  pagmo::ipopt ipopt;
+
+  // Set some of the IPOPT Wrapper settings.
+  // Notice that IPOPT only optimizes one of the population per evolve, so we set how we choose which one optimize
+  std::string type = "worst";
+  ipopt.set_selection(type);
+  //  ipopt.set_selection(0); // Can also select based on index as so
+  ipopt.set_replacement(type);
+
+  // Print the optimization results every 1 evaluation
+  ipopt.set_verbosity(1);
+
+  // 3 - Define a population to optimize
+  pagmo::population pop(pagmo_prob, 1);
+
+  // 4 - Evolve the population
+  for (int i = 0; i < 1; i++)
+  {
+    pop = ipopt.evolve(pop);
+    std::cout << "Champion x: " << pop.champion_x()[0] << "\n";
+    std::cout << "Champion f: " << pop.champion_f()[0] << "\n";
+  }
+}
